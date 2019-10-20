d008a99a86b2713a5e624878ee226a9526e798f6
naoki <duvingtdeux@gmail.com>
2019-10-20 14:01:29 +0900

    upated: userモデルのアソシエーションを書く順番

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :groups, through: :groups_users
-  has_many :messages
   has_many :groups_users
+  has_many :messages
 
 end
