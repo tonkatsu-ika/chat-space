aef545a56f99209f98494f3f51b0b1e803a1c5d9
naoki <duvingtdeux@gmail.com>
2019-10-21 12:05:53 +0900

    add: pry-byebug gem

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,17 +2,23 @@ class GroupsController < ApplicationController
 
 
   def new
+    @group = Group.new
   end
 
   def create
+    @group.create(group_params)
   end
 
   def edit
-    @group = Group.find(params[:id])
+    binding.pry
+    @group = Group.find(params[:id]).includes(:user)
   end
 
   def update
   end
 
-
+  private
+  def group_params
+    params.permit(:name)
+  end
 end
