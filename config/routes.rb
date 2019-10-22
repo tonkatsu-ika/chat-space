11006fd7be42e8cd49cf17847c12f64ed5932a8f
naoki <duvingtdeux@gmail.com>
2019-10-22 11:24:29 +0900

    update: change show to index re: messages routing

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :edit, :update]
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :users, only: [:edit, :update]
-    resources :messages, only: [:show, :create]
+    resources :messages, only: [:index, :create]
   end
 end
