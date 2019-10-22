c21febb8e629b646b44bc99575f497bba8aa6a8e
naoki <duvingtdeux@gmail.com>
2019-10-22 11:36:52 +0900

    add: actions to messages controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,20 @@
 class MessagesController < ApplicationController
   
   def index
+    @messages = Message.includes(:group).find(params[:group_id])
+  end
 
+  def create
+    if Message.create(message_params)
+      # 成功時
+    else
+      # 失敗時
+    end
+  end
+
+  private
+  def message_params
+    params.permit(:content, :image).merge(username: current_user.name)
   end
 
 
