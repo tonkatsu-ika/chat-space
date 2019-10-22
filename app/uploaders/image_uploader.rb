7d2a143d8c14492478f7e5c93cdd0c9c6fb08e31
naoki <duvingtdeux@gmail.com>
2019-10-22 12:09:30 +0900

    add: resize_to_fit in image_uploader

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,7 +1,7 @@
 class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -21,6 +21,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   # end
 
+  process resize_to_fit: [800, 800]
   # Process files as they are uploaded:
   # process scale: [200, 300]
   #
