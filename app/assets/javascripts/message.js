3acd3cd6d9ca86ff7af1c5806c4974f0a4255f9e
naoki <duvingtdeux@gmail.com>
2019-10-24 20:20:49 +0900

    add: rendering html function after posting a message

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,5 +1,25 @@
 $(document).ready(function() {
 
+  function buildHTML(data) {
+    let html = `<div class="chat-message">
+                  <div class="chat-massage__upper-content">
+                    <p class="chat-message__upper-content__user-name">
+                      ${data.user_name}
+                    </p>
+                    <p class="chat-message__upper-content__date">
+                      ${data.created_at}
+                    </p>
+                  </div>
+                  <p class="chat-message__message">
+                    ${data.content}
+                  </p>
+                  <img src=${data.image}>
+                </div>`;
+    
+    return html;
+
+  }
+
   
   $('#new_message').on('submit', function(e) {
     e.preventDefault();
@@ -14,8 +34,9 @@ $(document).ready(function() {
       contentType: false
     })
     .done(function(data){
-      console.log(data);
-      debugger
+      let html = buildHTML(data);
+      $('.chat-messages').append(html);
+      $('#message_content').val('');
     })
   });
 });
