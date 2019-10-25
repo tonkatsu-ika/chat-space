7cb7d9158d83970e83763471cf52dcb1e6d72d81
naoki <duvingtdeux@gmail.com>
2019-10-25 12:08:32 +0900

    fix: delete respond_to :html and simplify respond_to statement

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -12,10 +12,7 @@ class MessagesController < ApplicationController
     @message = @group.messages.new(message_params)
 
     if @message.save
-      respond_to do |format|
-        format.html { redirect_to group_messages_path }
-        format.json
-      end
+      respond_to :json
     else
       # 失敗時
       render :index
