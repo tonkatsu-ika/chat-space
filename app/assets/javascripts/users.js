db9866f18f94c242753e8fd283cbebe2990a22c2
naoki <duvingtdeux@gmail.com>
2019-10-26 13:44:48 +0900

    add: ajax通信成功後の一連の流れを実装（流れだけ）

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,5 +1,20 @@
 document.addEventListener('turbolinks:load', function() {
 
+  $.fn.addHTMLWhenNoUser = function(){
+    
+    var html = `<div>test</div>`;
+    
+    this.append(html);
+
+    console.log('func no user called');
+  }
+
+  $.fn.addHTMLWhenWithUsers = function(user){
+    
+    console.log('func with user called');
+
+  }
+
   $('#user-search-field').on('keyup', function() {
     let input = $(this).val();
     
@@ -10,9 +25,33 @@ document.addEventListener('turbolinks:load', function() {
       dataType: 'json',
       processData: false,
     })
-    .done(function() {
-      console.log('success');
+    .done(function(users) {
+      // emptyメソッドで一度検索結果をからにする
+      console.log($('#user-search-field').val());
+
+      $('#user-search-field').empty();
+      
+      console.log(`user # is ${users.length}`);
+
+      if (users.length === 0) {
+        console.log('no user');
+        // ユーザゼロの場合の処理
+        
+        $('#user-search-result').addHTMLWhenNoUser();
+
+      } else {
+        // 各ユーザの処理
+        users.forEach(function(user) {
+
+          $('#user-search-result').addHTMLWhenWithUsers(user);
+
+        });
+
+      }
+
     })
+    
+    
     .fail(function() {
       console.log('fail');
     })
