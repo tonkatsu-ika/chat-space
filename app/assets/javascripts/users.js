e0da51c91484f4ef22551894714b6e0e88cf42c3
naoki <duvingtdeux@gmail.com>
2019-10-26 12:55:41 +0900

    add user json

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -2,7 +2,21 @@ document.addEventListener('turbolinks:load', function() {
 
   $('#user-search-field').on('keyup', function() {
     let input = $(this).val();
-    console.log(input);
+    
+    $.ajax ({
+      url: 'http://localhost:3000/users',
+      method: 'GET',
+      data: input,
+      dataType: 'json',
+      processData: false,
+    })
+    .done(function() {
+      console.log('success');
+    })
+    .fail(function() {
+      console.log('fail');
+    })
+
   });
 
 });
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
 
   def index
+    return nil if params[:keyword] == ""
+    @users = User.where("name LIKE ?", "%#{params[:keyword]}%").limit(10)
     respond_to do |format|
       format.html
       format.json
