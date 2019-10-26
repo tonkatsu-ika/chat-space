d9c6bc5df88b5b98921c96f53c3ca97ab0058cb2
naoki <duvingtdeux@gmail.com>
2019-10-26 17:11:40 +0900

    fix: 入力値に応じた検索結果となるよう修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
 
   def index
-    return nil if params[:keyword] == ""
-    @users = User.where("name LIKE ?", "%#{params[:keyword]}%").where.not(id: current_user.id).limit(10)
+    return nil if params.keys[0] == ""
+    @users = User.where("name LIKE ?", "%#{params.keys[0]}%").where.not(id: current_user.id).limit(10)
     respond_to do |format|
       format.html
       format.json
