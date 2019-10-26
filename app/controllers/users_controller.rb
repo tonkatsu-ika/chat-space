7d1bff1f4adf2e7efc2f7aef5bdf2c1a76877e58
naoki <duvingtdeux@gmail.com>
2019-10-26 20:23:03 +0900

    fix: users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,6 @@ class UsersController < ApplicationController
     return nil if params.keys[0] == ""
     @users = User.where("name LIKE ?", "%#{params.keys[0]}%").where.not(id: current_user.id).limit(10)
     respond_to do |format|
-      format.html
       format.json
     end
   end
