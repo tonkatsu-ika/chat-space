21111934dd29304471e52874a0fab669da7fdc82
naoki <duvingtdeux@gmail.com>
2019-10-29 10:47:58 +0900

    add: unicorn and its settings

---
+++ config/unicorn.rb
@@ -0,0 +1,50 @@
+app_path = File.expand_path('../../', __FILE__)
+
+worker_processes 1
+
+# setting application directory
+working_directory app_path
+
+pid "#{app_path}/tmp/pids/unicorn.pid"
+
+# port
+listen 3000
+
+# error log
+stderrpath "#{app_path}/log/unicorn.stderr.log"
+
+# normal log
+stdout_path "#{app_path}/log/unicorn.stdout.log"
+
+# maximum time to waif for response
+timeout 60
+
+preload_app true
+GC.respond_to?(:copy_on_write_friendly=) && GC.copy_on_write_friendly = true
+
+check_client_connection false
+
+run_once = true
+
+before_fork do |server, worker|
+  defined?(ActiveRecord::Base) &&
+    ActiveRecord::Base.connection.disconnect!
+
+  if run_once
+    run_once = false # prevent from firing again
+  end
+
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if File.exist?(old_pid) && server.pid != old_pid
+    begin
+      sig = (worker.nr + 1) >= server.worker_process ? :QUIT : :TTOU
+      Process.kill(sig, File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errono::ESRCH => e
+      logger.error e
+    end
+  end
+end
+
+after_fork do |_server, _worker|
+  defined?(ActiveRecord::Base) && ActiveRecord::Base.establish_connection
+end
